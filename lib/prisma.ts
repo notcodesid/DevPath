6a03528c5596284b14e02ca5667dc95c286a59b0
siddharth <siddharth@siddharths-MacBook-Air.local>
2025-03-14 22:05:39 +0530

    fixed vercel build errors

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -4,10 +4,12 @@ import { PrismaClient } from '@prisma/client';
 // exhausting your database connection limit.
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
+// Ensure PrismaClient is properly instantiated for Vercel deployment
 export const prisma = 
   globalForPrisma.prisma || 
   new PrismaClient({
     log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
   });
 
+// Prevent multiple instances of Prisma Client in development
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma; 
